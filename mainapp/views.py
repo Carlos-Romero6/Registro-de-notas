e2490ac3eb9b1a9d87b225d8a70c2a5f8420df93
LudwingArandiaa <ludwingarandia26@gmail.com>
2024-12-13 11:53:32 -0400

    Logout library import on mainapp.views

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from .decorators import login_excluded
+from django.contrib.auth import logout
 from django.shortcuts import render
 from django.urls import reverse
 
