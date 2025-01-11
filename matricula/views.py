446a4441226dff257756431ee375922401156d40
Xixcrod <carlosdiaz200418@gmail.com>
2025-01-11 19:52:42 -0400

    añadido verificacion de eliminar secciones si hay estudiantes en las secciones a eliminar

--- matricula/views.py
+++ matricula/views.py
@@ -88,42 +88,65 @@ def secciones_matricula(request, id_matricula):#Vista del menú de secciones de 
         matricula = Matricula.objects.get(pk=id_matricula)
         pensums = Pensum.objects.all()#Obtenemos todos los pénsums para su seleccion en la ventana modal
         secciones = ['A', 'B', 'C', 'D', 'E', 'F'][:matricula.n_secciones]#Le asignamos una letra a cada sección de acuerdo a la cantidad de secciones en la matricula
-
         pensum = Pensum.objects.get(pk=matricula.pensum.id)
+
+        estudiantes = Estudiantes.objects.filter(matricula=matricula, estado=True)
+        # Diccionario para mapear las secciones a números
+        mapping = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6}
+
+        resultado = []
+        for estudiante in estudiantes:
+            valor_seccion = mapping.get(estudiante.seccion, None)
+            if valor_seccion:
+                resultado.append(valor_seccion)
+            else:
+                print(f"Sección inválida: {estudiante.seccion}")
+
+        if len(resultado) == 0:
+            resultado.append(0)
+        ult_seccion_estudiante = max(resultado)
+        print(ult_seccion_estudiante)
+
         return render(request, 'secciones_matricula.html', {
             'pensum': pensum,
             'matricula': matricula,
             'secciones': secciones,
-            'pensums': pensums
+            'pensums': pensums,
+            'ultima_seccion_disponible': ult_seccion_estudiante,
             })
 
 def modificarMatricula(request):#Vista que recibe una serie de datos de un formulario para modificar la matricula
     try:
         if request.method == 'POST':
-            print(request.POST)
-            #Asignamos variables para cada uno de los datos obtenidos del formulario
-            pensum_id_nuevo = request.POST.get('pensumnuevo_modificar')
-            print(pensum_id_nuevo)
-            pensum_id = request.POST.get('pensum_modificar')
-            print(pensum_id)
-            matricula_id = request.POST.get('matriculaid_modificar')
-            print(matricula_id)
-            curso = request.POST.get('curso_modificar')
-            promocion = request.POST.get('promocion_modificar')
             n_secciones = request.POST.get('secciones_modificar')
-            #Obtenemos el objeto del pénsum a modificar por su id
-            pensum = Pensum.objects.get(pk=pensum_id)
-            #Obtenemos el objeto del pénsum a colocar por su id
-            pensum_nuevo = Pensum.objects.get(pk=pensum_id_nuevo)
-            matricula = Matricula.objects.get(pk=matricula_id, pensum=pensum)
-            #Cambiamos sus atributos por los de las variables, y guardamos, si se logro devolvemos  al script un True sino devolvemos un False 
-            matricula.promocion = promocion
-            matricula.curso = curso
-            matricula.n_secciones = n_secciones
-            matricula.pensum = pensum_nuevo
-            matricula.save()
+            u_seccion = request.POST.get('ultima_secc_estudiante')
+            if n_secciones < u_seccion:
+                return JsonResponse({'success': False, 'message': 'Hay secciones que se eliminaran que aún contienen estudiantes'})
+
+            else:
+                print(request.POST)
+                #Asignamos variables para cada uno de los datos obtenidos del formulario
+                pensum_id_nuevo = request.POST.get('pensumnuevo_modificar')
+                print(pensum_id_nuevo)
+                pensum_id = request.POST.get('pensum_modificar')
+                print(pensum_id)
+                matricula_id = request.POST.get('matriculaid_modificar')
+                print(matricula_id)
+                curso = request.POST.get('curso_modificar')
+                promocion = request.POST.get('promocion_modificar')
+                n_secciones = request.POST.get('secciones_modificar')
+                #Obtenemos el objeto del pénsum a modificar por su id
+                pensum = Pensum.objects.get(pk=pensum_id)
+                #Obtenemos el objeto del pénsum a colocar por su id
+                pensum_nuevo = Pensum.objects.get(pk=pensum_id_nuevo)
+                matricula = Matricula.objects.get(pk=matricula_id, pensum=pensum)
+                #Cambiamos sus atributos por los de las variables, y guardamos, si se logro devolvemos  al script un True sino devolvemos un False 
+                matricula.promocion = promocion
+                matricula.curso = curso
+                matricula.n_secciones = n_secciones
+                matricula.pensum = pensum_nuevo
+                matricula.save()
         
-        return JsonResponse({'success': True, 'message': "La matricula ha sido modificada exitosamente."})
-    
+                return JsonResponse({'success': True, 'message': "La matricula ha sido modificada exitosamente."})
     except:
         return JsonResponse({'success': False, 'message': 'Ha ocurrido un fallo inesperado'})
