66552b96eb45d94f5d1e2f73a9c6927204768561
Xixcrod <carlosdiaz200418@gmail.com>
2026-09-14 13:41:23 -0400

    preparando para render part 7

---
+++ notas/utils/cursosDisponibles.py
@@ -0,0 +1,24 @@
+from mainapp.models import Matricula, Periodos
+
+# Obtiene las secciones disponibles
+def obtener():
+	# Constante con una tupla con cada uno de los nombres de los cursos
+    CURSOS_EXISTENTES = ("1er año", "2do año", "3er año", "4to año", "5to año")
+    # Función lambda para devolver el nombre del año en base al ID
+    numeroCurso = lambda indice: CURSOS_EXISTENTES[indice]
+    # Consulta cursos de las matriculas dentro del rango (1,5)
+    cursosDisponibles = set(Matricula.objects.filter(curso__range=(1,5)).values_list('curso', flat=True))
+
+    # Devolvera un diccionario con los cursos existentes
+    cursos = [ {"id": iterable + 1, "nombre": numeroCurso(iterable), "habilitado": (iterable + 1) in cursosDisponibles} for iterable in range(5)]
+    periodos_exist = Periodos.objects.exists()
+    return {'cursos' : cursos,
+            'periodos_exist': periodos_exist} # Devuelve los cursos obtenidos
+
+"""    for iterable in range(5):
+        curso = {
+            "id": iterable + 1,
+            "nombre": numeroCurso(iterable),
+            "habilitado": (iterable + 1) in cursosDisponibles
+        }
+        cursos.append(curso) """
